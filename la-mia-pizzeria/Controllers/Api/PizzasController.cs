be85fbd1188f8e92ba649dc5c8de30ed8063525c
Lorenzo <lorenzo.miccio97@gmail.com>
2022-05-19 15:14:09 +0200

    Completato esercizio base

---
+++ la-mia-pizzeria/Controllers/Api/PizzasController.cs
@@ -0,0 +1,22 @@
+﻿using la_mia_pizzeria.Dati;
+using Microsoft.AspNetCore.Mvc;
+using NuovaPizzeria.Models;
+
+namespace la_mia_pizzeria.Controllers.Api
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PizzasController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            List<Pizza> pizze = new List<Pizza>();
+            using (PizzaContext db = new PizzaContext())
+            {
+                pizze = db.Pizze.ToList<Pizza>();
+            }
+            return Ok(pizze);
+        }
+    }
+}
